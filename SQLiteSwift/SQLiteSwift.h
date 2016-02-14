d4498e17ae2aa905137a5dfe5383c56661b5675d
ohde-sg <ohde@socialgrid.co.jp>
2016-02-15 01:18:30 +0900

    Initial Commit

---
+++ SQLiteSwift/SQLiteSwift.h
@@ -0,0 +1,19 @@
+//
+//  SQLiteSwift.h
+//  SQLiteSwift
+//
+//  Created by 大出喜之 on 2016/02/15.
+//  Copyright © 2016年 yoshiyuki ohde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQLiteSwift.
+FOUNDATION_EXPORT double SQLiteSwiftVersionNumber;
+
+//! Project version string for SQLiteSwift.
+FOUNDATION_EXPORT const unsigned char SQLiteSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLiteSwift/PublicHeader.h>
+
+
